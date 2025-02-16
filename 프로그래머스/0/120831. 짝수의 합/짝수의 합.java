451cf83fbd59e4e111e5c7b827c85977655e768c
hahnjae29 <hahnjae29@gmail.com>
2025-02-16 20:25:20 +0900

    [level 0] Title: 짝수의 합, Time: 0.03 ms, Memory: 85 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while (n > 0){
+            if(n % 2 == 0){
+                answer += n;
+            } else {
+                n -= 1;
+                answer += n;
+            }
+            n -= 2;
+        }
+        return answer;
+    }
+}
