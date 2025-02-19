ce39d58d628f31ae9ff24e89f3889787b4403e86
hahnjae29 <hahnjae29@gmail.com>
2025-02-19 19:32:52 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.22 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,29 +1,43 @@
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 1;
         int max = 0;
-        int index = 0;
         for (int i = 0; i < numbers.length; i++){
-            if (numbers[i] > max){
-                max = numbers[i];
-                index = i;
-            }
-        }
-        
-        answer *= max;
-        max = 0;
-        
-        for (int i = 0; i < numbers.length; i++){
-            if (i == index){
-                continue;
-            } else {
-                if (numbers[i] > max){
-                    max = numbers[i];            
+            for (int j = 0; j < numbers.length; j++){
+                if (i == j){
+                    continue;
+                }
+                
+                if (numbers[i] * numbers[j] > max){
+                    max = numbers[i] * numbers[j];
                 }
             }
         }
+        return max;
         
-        answer *= max;
-        return answer;
+//         int answer = 1;
+//         int max = 0;
+//         int index = 0;
+//         for (int i = 0; i < numbers.length; i++){
+//             if (numbers[i] > max){
+//                 max = numbers[i];
+//                 index = i;
+//             }
+//         }
+        
+//         answer *= max;
+//         max = 0;
+        
+//         for (int i = 0; i < numbers.length; i++){
+//             if (i == index){
+//                 continue;
+//             } else {
+//                 if (numbers[i] > max){
+//                     max = numbers[i];            
+//                 }
+//             }
+//         }
+        
+//         answer *= max;
+//         return answer;
     }
 }
